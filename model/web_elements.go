f5a7730d115548e284af5c15554c4f1b7107c560
Kevin Klinger <kevthevirus@yahoo.de>
2021-04-01 10:30:30 +0200

    Extended FieldType to have subfields

--- model/web_elements.go
+++ model/web_elements.go
@@ -1,12 +1,19 @@
 package model
 
+// DynamicElement
 type DynamicElement struct {
 	Name   string      `json:"Name"`
 	Fields []FieldType `json:"Fields"`
 }
 
+// FieldType
 type FieldType struct {
-	Name     string `json:"Name"`
-	Type     string `json:"Type"`
-	Required bool   `json:"Required"`
+	Name          string       `json:"Name"`
+	Type          string       `json:"Type"`
+	ConflictsWith []string     `json:"ConflictsWith,omitempty"`
+	Required      bool         `json:"Required"`
+	Description   string       `json:"Description,omitempty"`
+	MaxItems      int          `json:"MaxItems,omitempty"`
+	MinItems      int          `json:"MinItems,omitempty"`
+	Subfields     *[]FieldType `json:"Subfields,omitempty"`
 }
--- router/kvm/general.go
+++ router/kvm/general.go
@@ -1,6 +1,7 @@
 package libvirt
 
 import (
+	"log"
 	"strings"
 
 	libvirtProvider "github.com/dmacvicar/terraform-provider-libvirt/libvirt"
@@ -12,7 +13,7 @@ var (
 	provider = libvirtProvider.Provider().(*schema.Provider)
 )
 
-// GetLibvirtFields returns all fields possible to create a libvirt disk pool
+// GetLibvirtFields returns a list of elements the terraform provider for libvirt supports
 func GetLibvirtFields() []model.DynamicElement {
 	var result []model.DynamicElement
 
@@ -30,15 +31,55 @@ func GetLibvirtFields() []model.DynamicElement {
 }
 
 func extractFields(rscAttr *schema.Resource) []model.FieldType {
+	var subfields *[]model.FieldType
 	fields := []model.FieldType{}
+
 	for fieldName, fieldAttributes := range rscAttr.Schema {
 		if !fieldAttributes.Computed && fieldName != "xml" {
+			if fieldAttributes.Elem != nil {
+				subfields = extractSubFields(fieldAttributes.Elem, fieldName)
+			}
+
 			fields = append(fields, model.FieldType{
-				Name:     fieldName,
-				Type:     strings.TrimLeft(fieldAttributes.Type.String(), "Type"),
-				Required: fieldAttributes.Required,
+				Name:          fieldName,
+				Type:          strings.TrimLeft(fieldAttributes.Type.String(), "Type"),
+				Required:      fieldAttributes.Required,
+				Subfields:     subfields,
+				ConflictsWith: fieldAttributes.ConflictsWith,
+				Description:   fieldAttributes.Description,
+				MinItems:      fieldAttributes.MinItems,
+				MaxItems:      fieldAttributes.MaxItems,
 			})
 		}
 	}
 	return fields
+}
+
+func extractSubFields(subfield interface{}, parentName string) *[]model.FieldType {
+	if elem, ok := subfield.(*schema.Resource); ok {
+		result := extractFields(elem)
+		if len(result) > 0 {
+			return &result
+		}
+		return nil
+	}
+
+	if element, ok := subfield.(*schema.Schema); ok {
+		if !element.Computed {
+			return &[]model.FieldType{
+				{
+					Type:          strings.TrimLeft(element.Type.String(), "Type"),
+					Required:      element.Required,
+					ConflictsWith: element.ConflictsWith,
+					Description:   element.Description,
+					MinItems:      element.MinItems,
+					MaxItems:      element.MaxItems,
+				},
+			}
+		}
+		return nil
+	}
+
+	log.Printf("Error converting subelement of %s to schema.Resource \n", parentName)
+	return nil
 }
