ce7a44e428ca9d5c11dee4ef1166a75b86e6f9bb
morteza <hosseini@fzi.de>
2022-11-29 22:06:28 +0100

    destroy Terraform

--- router/apis.go
+++ router/apis.go
@@ -2,10 +2,12 @@ package router
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/kevinklinger/consilio/internal/pkg/statemanager"
 	"github.com/kevinklinger/consilio/model"
 	"github.com/kevinklinger/consilio/router/alicloud"
 	libvirt "github.com/kevinklinger/consilio/router/kvm"
@@ -41,5 +43,40 @@ func (s *ConsilioRouter) handleGetAPI() httprouter.Handle {
 			log.Panicln(errors.WithStack(err))
 		}
 		_, _ = rw.Write(output)
+	}
+}
+
+func (s *ConsilioRouter) deleteImportStateHandler() httprouter.Handle {
+	return func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		err := request.ParseMultipartForm(10 * 1024 * 1024)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, _ = writer.Write([]byte(err.Error()))
+			return
+		}
+
+		stateFile, _, err := request.FormFile("stateFile")
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, _ = writer.Write([]byte(err.Error()))
+			return
+		}
+		defer stateFile.Close()
+
+		stateFileContent, err := io.ReadAll(stateFile)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, _ = writer.Write([]byte(err.Error()))
+			return
+		}
+
+		err = statemanager.DeleteState(stateFileContent)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, _ = writer.Write([]byte(err.Error()))
+			return
+		}
+
+		writer.WriteHeader(http.StatusNoContent)
 	}
 }
